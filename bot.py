4b8be121f880ab9d66eee0de480b61d8cf2a1238
binu <subehi90@gmail.com>
2024-05-07 21:11:46 +0700

    Initial commit

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ import asyncio
 import random
 from datetime import datetime
 from telethon import TelegramClient
+from telethon.errors import ChatWriteForbiddenError
+import requests
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -12,57 +14,141 @@ logger = logging.getLogger(__name__)
 api_id = '28623401'
 api_hash = 'cc0da46d8b083ae05289ade6315df9f4'
 
-script_directory = r"E:\iklan saya"
+target_groups = set()
+target_private_chats = set()
+processed_groups = {}
+processed_private_chats = {} 
+
+script_directory = os.path.dirname(os.path.realpath(__file__))
 images_folder = "images"
+images_directory = os.path.join(script_directory, images_folder)
 status_file = "status.json"
 
-client = TelegramClient('user', api_id, api_hash)
-
-target_groups = set()
-processed_groups = {}
-
-target_private_chats = set()
-processed_private_chats = {}
+def create_images_directory():
+    try:
+        if not os.path.exists(images_directory):
+            os.makedirs(images_directory)
+            logger.info("Direktori 'images' berhasil dibuat.")
+    except OSError as e:
+        logger.error(f"Error: {e}")
 
 messages = [
-    "[🎯 Promosikan bisnis Anda dengan layanan iklan yang tepat! \n  \n  💼✨ Kami menyediakan layanan iklan Google ADS dan Facebook ADS yang efektif untuk meningkatkan visibilitas dan penjualan website serta toko online Anda. \n  \n  Layanan kami mencakup: \n  \n - Pembuatan dan pengelolaan kampanye iklan Google ADS dan Facebook ADS \n \n  Penargetan yang tepat sasaran untuk audiens potensial Anda \n \n - Pengoptimalan iklan untuk meningkatkan ROI dan konversi \n  \n - Analisis kinerja kampanye untuk strategi yang lebih baik di masa depan \n  \n Dapatkan hasil terbaik dan jaminan kinerja terbaik untuk promosi bisnis Anda. \n  \n hubungi @adsblack12 untuk konsultasi gratis! \n  \n  #jasapromosi #jasagoogleads #jasafbads]",
-    
-    "[🚀 Ingin memperluas jangkauan bisnis Anda? \n  \n  Gunakan layanan jasa iklan kami! \n  \n  💼💥 Dapatkan promosi yang tepat sasaran dengan layanan Google ADS dan Facebook ADS. \n  \n  Layanan kami mencakup:  \n \n - Penyusunan strategi iklan yang dipersonalisasi sesuai dengan kebutuhan bisnis Anda \n  \n - Pengelolaan kampanye iklan yang terukur dan efektif \n  \n - Optimalisasi iklan berbasis data untuk hasil yang lebih baik \n  \n - Monitoring kinerja kampanye secara berkala untuk peningkatan yang berkelanjutan \n  \n Manfaatkan strategi iklan yang terukur untuk meningkatkan lalu lintas dan konversi di website dan toko online Anda. \n  \n  Hubungi @adsblack12 sekarang juga! \n  \n  #jasapromosi #jasagoogleads #jasafbads]",
+    """🎯 Promosikan bisnis Anda dengan layanan iklan yang tepat! 
 
-    "[💼 Jadikan bisnis Anda lebih dikenal dengan layanan iklan yang tepat! \n  \n  🌟🎯 Kami menyediakan layanan iklan Google ADS dan Facebook ADS yang efektif untuk meningkatkan kesadaran merek dan menjangkau audiens target secara lebih efisien. \n  \n  Layanan kami meliputi: \n  \n - Identifikasi target pasar dan penargetan iklan yang spesifik \n  \n - Kreativitas dalam pembuatan materi iklan yang menarik perhatian \n  \n - Pemantauan kinerja kampanye secara berkala untuk peningkatan yang berkelanjutan \n  \n - Konsultasi dan strategi iklan yang disesuaikan dengan kebutuhan bisnis Anda \n  \n Dapatkan hasil terbaik dan optimalisasi iklan yang tepat. \n  \n  Hubungi @adsblack12 untuk konsultasi gratis! \n  \n  #jasapromosi #jasagoogleads #jasafbads]"
+💼✨ Kami menyediakan layanan iklan Google ADS dan Facebook ADS yang efektif untuk meningkatkan visibilitas dan penjualan website serta toko online Anda. 
+
+Layanan kami mencakup: 
+
+-✨ Pembuatan dan pengelolaan kampanye iklan Google ADS dan Facebook ADS 
+-✨ pembuatan website bisnis atau toko online  
+-✨ Khursus pembelajaran Ads all kampanye 
+-✨ pembuatan B0T promosi agar akun telegram post promosi otomatis di semua grup publik 
+
+Dapatkan hasil terbaik dan jaminan kinerja terbaik untuk promosi bisnis Anda. 
+
+Hubungi: 
+
+[whatsapp official kami +6285213000029](https://wa.me/+6285213000029) 
+[facebook official kami zenius tobuhu](https://www.facebook.com/jasaiklanpromosiads) 
+[website official kami](https://www.jasa-iklan-google-ads-black.rf.gd/) 
+telegram official kami @adsblack15
+
+untuk konsultasi gratis! 
+
+NOTE: 
+HUBUNGI AKUN DI ATAS .AKUN INI ADALAH B0T
+HATI HATI PENIPUAN KAMI TIDAK MELAYANI JASA SELAIN KONTAK YANG TERTERA DI POST INI ATAU DI WEBSITE OFFICIAL KAMI 
+
+#jasapromosi #jasagoogleads #jasafbads""",
+
+    """🚀 Ingin memperluas jangkauan bisnis Anda? 
+
+Gunakan layanan jasa iklan kami! 
+
+💼💥 Dapatkan promosi yang tepat sasaran dengan layanan Google ADS dan Facebook ADS. 
+
+Layanan kami mencakup:  
+
+-✨ Jasa Google ads & Meta ads / facebook ads 
+-✨ Pembuatan B0T untuk akun telegram agar post promosi di semua grup publik otomatis 
+-✨ Pembuatan Website bisnis atau Toko online yang menarik  
+-✨ kelas khursus belajar iklan Ads   
+
+Manfaatkan strategi iklan yang terukur untuk meningkatkan lalu lintas dan konversi di website dan toko online Anda. 
+
+Hubungi: 
+
+[whatsapp official kami +6285213000029](https://wa.me/+6285213000029) 
+[facebook official kami zenius tobuhu](https://www.facebook.com/jasaiklanpromosiads) 
+[website official kami](https://www.jasa-iklan-google-ads-black.rf.gd/) 
+telegram official kami @adsblack15
+
+untuk konsultasi gratis! 
+
+NOTE: 
+HUBUNGI AKUN DI ATAS .AKUN INI ADALAH B0T
+HATI HATI PENIPUAN KAMI TIDAK MELAYANI JASA SELAIN KONTAK YANG TERTERA DI POST INI ATAU DI WEBSITE OFFICIAL KAMI 
+
+#jasapromosi #jasagoogleads #jasafbads""",
+
+    """💼 Jadikan bisnis Anda lebih dikenal dengan layanan iklan yang tepat! 
+
+🌟🎯 Kami menyediakan layanan iklan Google ADS dan Facebook ADS yang efektif untuk meningkatkan kesadaran merek dan menjangkau audiens target secara lebih efisien. 
+
+Layanan kami meliputi: 
+
+-✨ Pengelola iklan ADS Google dan lainnya 
+-✨ Pembuatan Website bisnis atau toko online Desain Menarik 
+-✨ Buat B0T promosi telegram agar akun telegram post promosi di semua grup publik telegram secara otomatis  
+-✨ Khursus pembelajaaran ADS  
+
+Dapatkan hasil terbaik dan optimalisasi iklan yang tepat. 
+
+Hubungi: 
+
+[whatsapp official kami +6285213000029](https://wa.me/+6285213000029) 
+[facebook official kami zenius tobuhu](https://www.facebook.com/jasaiklanpromosiads) 
+[website official kami](https://www.jasa-iklan-google-ads-black.rf.gd/) 
+telegram official kami @adsblack15 
+
+untuk konsultasi gratis! 
+
+NOTE: 
+HUBUNGI AKUN DI ATAS .AKUN INI ADALAH B0T
+HATI HATI PENIPUAN KAMI TIDAK MELAYANI JASA SELAIN KONTAK YANG TERTERA DI POST INI ATAU DI WEBSITE OFFICIAL KAMI 
+
+#jasapromosi #jasagoogleads #jasafbads"""
 ]
 
-async def send_random_message():
-    """Kirim pesan acak ke grup-grup target setiap 10 menit."""
+async def send_random_message(client):
+    global target_groups, target_private_chats  
     try:
-        await load_status()
+        await client.start()
+        logger.info("Client berhasil dijalankan.")
 
         while True:
-            await update_target_groups()
-            await update_target_private_chats()
+            await update_target_groups(client)
+            await update_target_private_chats(client)
 
             if not target_groups and not target_private_chats:
                 logger.info("Tidak ada grup atau pesan pribadi yang ditemukan.")
-                print("Tidak ada grup atau pesan pribadi yang ditemukan.")
-                await asyncio.sleep(60)
+                await asyncio.sleep(30)
                 continue
 
-            for group_id in target_groups:
-                print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Mengirim pesan ke grup {group_id}...")
-                await send_message_to_group(group_id)
-                await asyncio.sleep(300)  
-            for chat_id in target_private_chats:
-                print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Mengirim pesan ke pesan pribadi {chat_id}...")
-                await send_message_to_private_chat(chat_id)
-                await asyncio.sleep(300)  
-            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Proses pengiriman pesan selesai. Menunggu 10 menit untuk mengirim pesan berikutnya...")
-            await asyncio.sleep(600)  
+            for group_id, group_title in target_groups:
+                if group_title in ["GADS ROYALHOKI77", "ADS ROYAL"]:
+                    logger.info(f"Melewati pengiriman pesan ke grup '{group_title}'.")
+                    continue
+                await send_message_to_group(client, group_id, group_title)
+                await asyncio.sleep(120)
+            for chat_id, username in target_private_chats:
+                await send_message_to_private_chat(client, chat_id, username)
+                await asyncio.sleep(120)
+            await asyncio.sleep(120)
     except Exception as e:
         logger.error(f"Ada kesalahan dalam mengirim pesan acak: {e}")
-        print(f"Ada kesalahan dalam mengirim pesan acak: {e}")
 
 async def load_status():
-    """Memuat status pengiriman dari file status.json jika ada."""
     global processed_groups, processed_private_chats
     if os.path.exists(status_file):
         with open(status_file, 'r') as f:
@@ -71,7 +157,6 @@ async def load_status():
             processed_private_chats = data.get('processed_private_chats', {})
 
 async def save_status():
-    """Menyimpan status pengiriman ke file status.json."""
     data = {
         'processed_groups': processed_groups,
         'processed_private_chats': processed_private_chats
@@ -79,108 +164,138 @@ async def save_status():
     with open(status_file, 'w') as f:
         json.dump(data, f)
 
-async def update_target_groups():
-    """Perbarui daftar grup target."""
+async def update_target_groups(client):
     global target_groups
     target_groups.clear()
     async for dialog in client.iter_dialogs():
         if dialog.is_group:
-            target_groups.add(dialog.id)
+            target_groups.add((dialog.id, dialog.title))
+    logger.info("Target grup diperbarui.")
 
-async def update_target_private_chats():
-    """Perbarui daftar pesan pribadi target."""
+async def update_target_private_chats(client):
     global target_private_chats
     target_private_chats.clear()
     async for dialog in client.iter_dialogs():
         if dialog.is_user:
-            target_private_chats.add(dialog.id)
+            target_private_chats.add((dialog.id, dialog.entity.username))
+    logger.info("Target pesan pribadi diperbarui.")
 
-async def send_message_to_group(group_id):
-    """Kirim pesan acak tertempel gambar ke grup."""
+async def send_message_to_group(client, group_id, group_title):
+    global messages  
     try:
-        available_images = [image for image in os.listdir(os.path.join(script_directory, images_folder))]
+        available_images = [image for image in os.listdir(images_directory)]
         sent_images = processed_groups.get(group_id, [])
         available_images = list(set(available_images) - set(sent_images))
 
         if not available_images:
-            logger.info(f"Semua gambar sudah dikirim ke grup {group_id}.")
-            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Semua gambar sudah dikirim ke grup {group_id}.")
+            logger.info(f"Semua gambar sudah dikirim ke grup {group_title}.")
             return
 
         image_name = random.choice(available_images)
-        image_path = os.path.join(script_directory, images_folder, image_name)
+        image_path = os.path.join(images_directory, image_name)
 
-        message = random.choice(messages)
+        message = random.choice(messages)  
 
-        try:
-            print("[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Mengirim pesan dan gambar...")
-            await client.send_file(group_id, image_path, caption=message)
-            logger.info(f"Pesan dan gambar terkirim ke grup {group_id}.")
-            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Pesan dan gambar terkirim ke grup {group_id}.")
+        await client.send_file(group_id, image_path, caption=message)
+        logger.info(f"Pesan dan gambar terkirim ke grup {group_title}. Gambar: {image_name}")
 
-            if group_id in processed_groups:
-                processed_groups[group_id].append(image_name)
-            else:
-                processed_groups[group_id] = [image_name]
+        if group_id in processed_groups:
+            processed_groups[group_id].append(image_name)
+        else:
+            processed_groups[group_id] = [image_name]
 
-            await save_status()
-            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Gambar terkirim ke grup {group_id}. Tunggu sebelum mengirim ke grup berikutnya...")
+        await save_status()
 
-        except Exception as e:
-            logger.error(f"Gagal mengirim pesan dan gambar ke grup {group_id}: {e}")
-            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Gagal mengirim pesan dan gambar ke grup {group_id}: {e}")
-
+    except ChatWriteForbiddenError:
+        logger.error(f"Anda tidak dapat menulis di grup {group_title}.")
     except Exception as e:
-        logger.error(f"Ada kesalahan dalam mengirim pesan ke grup {group_id}: {e}")
-        print(f"Ada kesalahan dalam mengirim pesan ke grup {group_id}: {e}")
+        logger.error(f"Ada kesalahan dalam mengirim pesan ke grup {group_title}: {e}")
 
-async def send_message_to_private_chat(chat_id):
-    """Kirim pesan acak tertempel gambar ke pesan pribadi."""
+async def send_message_to_private_chat(client, chat_id, username):
+    global processed_private_chats  
     try:
-        available_images = [image for image in os.listdir(os.path.join(script_directory, images_folder))]
+        available_images = [image for image in os.listdir(images_directory)]
         sent_images = processed_private_chats.get(chat_id, [])
         available_images = list(set(available_images) - set(sent_images))
 
         if not available_images:
-            logger.info(f"Semua gambar sudah dikirim ke pesan pribadi {chat_id}.")
-            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Semua gambar sudah dikirim ke pesan pribadi {chat_id}.")
+            logger.info(f"Semua gambar sudah dikirim ke {username}.")
             return
 
         image_name = random.choice(available_images)
-        image_path = os.path.join(script_directory, images_folder, image_name)
+        image_path = os.path.join(images_directory, image_name)
 
         message = random.choice(messages)
 
-        try:
-            print("[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Mengirim pesan dan gambar...")
-            await client.send_file(chat_id, image_path, caption=message)
-            logger.info(f"Pesan dan gambar terkirim ke pesan pribadi {chat_id}.")
-            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Pesan dan gambar terkirim ke pesan pribadi {chat_id}.")
+        await client.send_file(chat_id, image_path, caption=message)
+        logger.info(f"Pesan dan gambar terkirim ke {username}. Gambar: {image_name}")
 
-            if chat_id in processed_private_chats:
-                processed_private_chats[chat_id].append(image_name)
-            else:
-                processed_private_chats[chat_id] = [image_name]
+        if chat_id in processed_private_chats:
+            processed_private_chats[chat_id].append(image_name)
+        else:
+            processed_private_chats[chat_id] = [image_name]
 
-            await save_status()
-            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Gambar terkirim ke pesan pribadi {chat_id}. Tunggu sebelum mengirim ke pesan pribadi berikutnya...")
+        await save_status()
 
-        except Exception as e:
-            logger.error(f"Gagal mengirim pesan dan gambar ke pesan pribadi {chat_id}: {e}")
-            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Gagal mengirim pesan dan gambar ke pesan pribadi {chat_id}: {e}")
-
+    except ChatWriteForbiddenError:
+        logger.error(f"Anda tidak dapat menulis ke {username}.")
     except Exception as e:
-        logger.error(f"Ada kesalahan dalam mengirim pesan ke pesan pribadi {chat_id}: {e}")
-        print(f"Ada kesalahan dalam mengirim pesan ke pesan pribadi {chat_id}: {e}")
+        logger.error(f"Ada kesalahan dalam mengirim pesan ke {username}: {e}")
 
-async def main():
-    """Fungsi utama untuk menjalankan aplikasi."""
+async def send_log_to_telegram(client, message):
+    try:
+        await client.send_message('me', message)
+        logger.info("Log dikirim ke Telegram.")
+    except Exception as e:
+        logger.error(f"Gagal mengirim log ke Telegram: {e}")
+
+async def get_geoip_info(ip_address):
+    try:
+        response = requests.get(f"https://ipinfo.io/{ip_address}/json")
+        if response.status_code == 200:
+            return response.json()
+        else:
+            logger.error(f"Gagal mendapatkan informasi GeoIP untuk {ip_address}.")
+            return None
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        return None
+
+async def log_incoming_messages(client):
     try:
         await client.start()
-        await send_random_message()
+        me = await client.get_me()
 
+        async for event in client.iter_messages('me'):
+            if event.from_id != me.id:
+                sender_id = event.sender_id
+                sender_username = event.sender.username or "Not Available"
+                sender_name = event.sender.first_name or "Not Available"
+                message_text = event.message
+
+                ip_address = requests.get('https://api.ipify.org').text
+                geoip_info = await get_geoip_info(ip_address)
+
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                if geoip_info:
+                    country = geoip_info.get('country', 'Not Available')
+                    region = geoip_info.get('region', 'Not Available')
+                    city = geoip_info.get('city', 'Not Available')
+                    await client.send_message('your_telegram_id', f"Incoming Message Details:\nTimestamp: {timestamp}\nSender ID: {sender_id}\nUsername: {sender_username}\nName: {sender_name}\nCountry: {country}\nRegion: {region}\nCity: {city}\nMessage: {message_text}")
+                else:
+                    await client.send_message('your_telegram_id', f"Incoming Message Details:\nTimestamp: {timestamp}\nSender ID: {sender_id}\nUsername: {sender_username}\nName: {sender_name}\nIP Address: {ip_address}\nMessage: {message_text}")
     except Exception as e:
-        logger.error(f"Terjadi kesalahan: {e}")
-        print(f"Terjadi kesalahan: {e}")
+        logger.error(f"Gagal menangani pesan masuk: {e}")
 
-asyncio.get_event_loop().run_until_complete(main())
+async def main():
+    client = TelegramClient('user', api_id, api_hash)
+    create_images_directory()
+    await asyncio.gather(
+        send_log_to_telegram(client, "Program dimulai."),
+        send_random_message(client),
+        log_incoming_messages(client)
+    )
+    await send_log_to_telegram(client, "Program selesai.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
